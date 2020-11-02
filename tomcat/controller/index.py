480943be42482ba14e9ff45e896b5efc5a347cbe
HadLuo <31942443+HadLuo@users.noreply.github.com>
2020-11-02 17:18:19 +0800

    Update index.py

--- tomcat/controller/index.py
+++ tomcat/controller/index.py
@@ -4,13 +4,4 @@ from tomcat.controller.postman import *
 def service(request):
     body = request.body()
     print("收到index请求,p:" + str(body))
-    postman = PostMan()
-    postman.set_token()
-    if body['type'] == '0':
-        ret = postman.redis(body['param'])
-        return {'code': 0, 'data': ret}
-    elif body['type'] == '1':
-        ret = postman.sql(body['param'])
-        return {'code': 0, 'data': ret}
-    else:
-        return {'code': -1, 'err': '不支持的操作'}
+    return {'code': 0, 'data': '请求成功'}
